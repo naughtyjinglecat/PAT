1c574cc7fb05efa1ff854ce86fb788f4355fb4f4
naughtyjinglecat <deepness_rounder@163.com>
2022-05-08 18:10:12 +0800

    2022.05.08,不会,跳过,以后回来补上

---
+++ A1049.Counting_Ones/A1049.Counting_Ones.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+
+
+/*啥子题哦，暂时略过，后期回来补上*/
+
+int main(void){
+    // char n[11];
+    // scanf("%s", n);
+    // printf("%d\n", countOnes(n));
+    return 0;
+}
