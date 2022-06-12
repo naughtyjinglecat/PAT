d4ecce15eeb4c5b681e87579311e66b886771200
naughtyjinglecat <deepness_rounder@163.com>
2022-06-12 10:01:02 +0800

    2022.6.12,22分

--- B1025.反转链表/B1025.反转链表.cpp
+++ B1025.反转链表/B1025.反转链表.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 //由于无效节点没有剔除，所以21分。。。
+//矫正之后也是22分，还有未知问题。。。
 
 
 typedef struct node{
@@ -15,21 +16,24 @@ typedef struct node{
 
 int main(void){
     node nodes[100000];
+    int i = 0;
     int temp_adress;
     int head, n, k;
-    int *addresses;//记录节点的地址顺序
+    int addresses[100];//记录节点的地址顺序
     int p;//指向当前节点
     scanf("%d %d %d", &head, &n, &k);
-    addresses = (int*)malloc(sizeof(int)*n);
+    // addresses = (int*)malloc(sizeof(int)*n);
     for(int i = 0; i < n; i++){
         scanf("%d", &temp_adress);
         scanf("%d %d", &nodes[temp_adress].data, &nodes[temp_adress].next);
     }
     p = head;
-    for(int i = 0; i < n; i++){
+    for(i = 0; i < n && p >= 0; i++){
         addresses[i] = p;
         p = nodes[p].next;
     }
+    //修改n的值，此时n的值为有效节点的个数
+    n = i;
     //修改访问顺序
     for(int i = 0; i < n; i += k){
         if(i+k-1 <= n){
